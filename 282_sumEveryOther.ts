6ec97ef3c89e1301099b44cec69d17ee57b223ff
Stefan Kernjak <stefan@thisotherthing.co>
2023-01-09 19:47:26 +0100

    282

---
+++ 282_sumEveryOther.ts
@@ -0,0 +1,19 @@
+import { assertEquals } from "https://deno.land/std@0.161.0/testing/asserts.ts";
+
+const sumEveryOther = (val: number): number => {
+  const str = val.toString().replace(/\./, "");
+
+  let sum = 0;
+
+  for (let i = 1, l = str.length; i < l; i += 2) {
+    sum += parseInt(str[i], 10);
+  }
+
+  return sum;
+};
+
+Deno.test("truncate", () => {
+  assertEquals(sumEveryOther(548915381), 26); // 4+9+5+8
+  assertEquals(sumEveryOther(10), 0);
+  assertEquals(sumEveryOther(1010.11), 1); // 0+0+1
+});
