7dee0d69e20be402bc57449774ed9205888f7448
Stefan Kernjak <stefan@thisotherthing.co>
2023-02-13 21:04:41 +0100

    286

---
+++ 286_columnNames.ts
@@ -0,0 +1,25 @@
+import { assertEquals } from "https://deno.land/std@0.161.0/testing/asserts.ts";
+
+const func = (val: string): number => {
+  let index = 0;
+  let mult = 1;
+
+  for (let i = val.length - 1; i >= 0; i--) {
+    index += mult * (val.charCodeAt(i) - 64);
+
+    mult *= 26;
+  }
+
+  return index;
+};
+
+Deno.test("test", () => {
+  assertEquals(func("A"), 1);
+  assertEquals(func("B"), 2);
+  assertEquals(func("C"), 3);
+  assertEquals(func("Z"), 26);
+  assertEquals(func("AA"), 27);
+  assertEquals(func("AB"), 28);
+  assertEquals(func("ZZ"), 702);
+  assertEquals(func("AAA"), 703);
+});
